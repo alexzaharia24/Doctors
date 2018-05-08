9a915e452bf1479534a881af532c948145cbefab
Alex <alex.zaharia24@gmail.com>
2018-05-09 01:22:37 +0300

    Mocked test.

--- src/test/java/AddInvalidConsultation.java
+++ src/test/java/AddInvalidConsultation.java
@@ -1,23 +1,51 @@
 import controller.DoctorController;
 import exceptions.ConsultationException;
+import model.Consultation;
 import model.Patient;
 import org.junit.Assert;
 import org.junit.Test;
 import repository.Repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.when;
+
 public class AddInvalidConsultation {
+    private Repository repo;
+    private DoctorController ctrl;
+    private List<Consultation> consultations;
+    private List<Patient> patients;
+
+    public void initialize() {
+        repo = mock(Repository.class);
+
+        consultations = new ArrayList<Consultation>();
+        Consultation c1 = new Consultation("6", "1234567890123", "Flu", Collections.singletonList("Tea"), "10-04-2018");
+        Consultation c2 = new Consultation("7", "2234567890123", "Flu", Collections.singletonList("Tea"), "25-04-2018");
+        consultations.add(c1);
+        consultations.add(c2);
+
+        patients = new ArrayList<Patient>();
+        Patient p1 = new Patient("James", "1234567890123", "Broadway Str.");
+        Patient p2 = new Patient("Sally", "2234567890123", "Rainbow Avenue");
+        patients.add(p1);
+        patients.add(p2);
+
+        when(repo.getConsultationList()).thenReturn(consultations);
+        when(repo.getPatientList()).thenReturn(patients);
+
+        ctrl = new DoctorController(repo);
+    }
+
     @Test
     public void addConsultation() {
-        String patients = "TestFilePatients.txt";
-        String consultations = "TestFileConsultations.txt";
-        Repository repo = new Repository(patients, consultations);
-        DoctorController ctrl = new DoctorController(repo);
+        initialize();
 
-        int nrConsultations = ctrl.getConsultationList().size();
-
+        int nrConsultations = consultations.size();
         int idConsultation = Integer.parseInt(ctrl.getConsultationList().get(nrConsultations-1).getConsID()) + 1;
         List<String> meds = new ArrayList<String>();
         meds.add("No med");
@@ -28,7 +56,7 @@ public class AddInvalidConsultation {
 
         }
 
-        Assert.assertEquals(ctrl.getConsultationList().size(), nrConsultations );
+        Assert.assertEquals(consultations.size(), nrConsultations );
 
     }
 }
--- src/test/java/AddPatientBBT.java
+++ src/test/java/AddPatientBBT.java
@@ -1,173 +1,176 @@
 import controller.DoctorController;
 import exceptions.PatientException;
+import model.Consultation;
 import model.Patient;
 import org.junit.Assert;
 import org.junit.Test;
 import repository.Repository;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AddPatientBBT {
     private String filePatients = "TestFilePatients.txt";
     private String fileConsultations = "TestFileConsultations.txt";
 
+    private Repository repo;
+    private DoctorController ctrl;
+    private List<Consultation> consultations;
+    private List<Patient> patients;
+
+    public void initialize() {
+        repo = mock(Repository.class);
+
+        consultations = new ArrayList<Consultation>();
+        Consultation c1 = new Consultation("3", "1234567890123", "Flu", Collections.singletonList("Tea"), "10-04-2018");
+        Consultation c2 = new Consultation("4", "2234567890123", "Flu", Collections.singletonList("Tea"), "25-04-2018");
+        consultations.add(c1);
+        consultations.add(c2);
+
+        patients = new ArrayList<Patient>();
+        Patient p1 = new Patient("James", "1234567890123", "Broadway Str.");
+        Patient p2 = new Patient("Sally", "2234567890123", "Rainbow Avenue");
+        patients.add(p1);
+        patients.add(p2);
+
+        when(repo.getConsultationList()).thenReturn(consultations);
+        when(repo.getPatientList()).thenReturn(patients);
+
+        ctrl = new DoctorController(repo);
+    }
+
     @Test
     public void test_tc_1_AllValid() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
+        initialize();
 
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        int initialSize = patients.size();
         Patient p = new Patient("Tom", "1234567890123", "Home");
         try {
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize+1);
+        Assert.assertEquals(patients.size(), initialSize + 1);
     }
 
     @Test
     public void test_tc_2_CNPOnlyDigitsTooShort() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
-
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        initialize();
+        int initialSize = patients.size();
         Patient p = new Patient("Tom", "100", "Home");
         try {
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize);
+        Assert.assertEquals(patients.size(), initialSize);
     }
 
     @Test
     public void test_tc_3_CNPDigitsAndChars() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
-
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        initialize();
+        int initialSize = patients.size();
         Patient p = new Patient("Tom", "100a", "Home");
         try {
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize);
+        Assert.assertEquals(patients.size(), initialSize);
     }
 
     @Test
     public void test_tc_4_NameInvalidChars() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
-
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        initialize();
+        int initialSize = patients.size();
         try {
             Patient p = new Patient("Tom$", "1990825060078", "Home");
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize);
+        Assert.assertEquals(patients.size(), initialSize);
     }
 
     @Test
     public void test_tc_5_AddressInvalidChars() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
-
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        initialize();
+        int initialSize = patients.size();
         try {
             Patient p = new Patient("Tom", "1990825060078", "Home$");
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize);
+        Assert.assertEquals(patients.size(), initialSize);
     }
 
     @Test
     public void test_tc_6_NameEmpty() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
-
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        initialize();
+        int initialSize = patients.size();
         try {
             Patient p = new Patient("", "1990825060078", "Home");
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize);
+        Assert.assertEquals(patients.size(), initialSize);
     }
 
     @Test
     public void test_tc_7_AddressEmpty() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
-
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        initialize();
+        int initialSize = patients.size();
         try {
             Patient p = new Patient("Tom", "1990825060078", "");
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize);
+        Assert.assertEquals(patients.size(), initialSize);
     }
 
     @Test
     public void test_tc_8_CNP12Digits() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
-
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        initialize();
+        int initialSize = patients.size();
         try {
             Patient p = new Patient("Tom", "123456789012", "Home");
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize);
+        Assert.assertEquals(patients.size(), initialSize);
     }
 
     @Test
     public void test_tc_9_CNP14Digits() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
-
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        initialize();
+        int initialSize = patients.size();
         try {
             Patient p = new Patient("Tom", "12345678901234", "Home");
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize);
+        Assert.assertEquals(patients.size(), initialSize);
     }
 
     @Test
     public void test_tc_10_Name1ValidChar() {
-        Repository repo = new Repository(filePatients, fileConsultations);
-        DoctorController ctrl = new DoctorController(repo);
-
-        List<Patient> pList = ctrl.getPatientList();
-        int initialSize = pList.size();
+        initialize();
+        int initialSize = patients.size();
         try {
             Patient p = new Patient("T", "1234567890123", "Home");
             ctrl.addPatient(p);
         } catch (PatientException e) {
         }
 
-        Assert.assertEquals(pList.size(), initialSize+1);
+        Assert.assertEquals(patients.size(), initialSize + 1);
     }
 }
